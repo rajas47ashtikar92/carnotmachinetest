ab65b44c3d837a30103cef45c729d02d1825005c
Rajas Ashtikar <r47@Rajass-MacBook-Pro.local>
2017-10-29 16:06:10 +0530

    Created the utility for getting the data from the server

---
+++ app/src/main/java/com/developer/r47/carnotmachinetest/utilities/DownloadingApi.java
@@ -0,0 +1,14 @@
+package com.developer.r47.carnotmachinetest.utilities;
+
+/**
+ * Created by r47 on 29/10/17.
+ * This class is the api class, this contains the endpoints that the application will
+ * hit, in order to get the required data from the server
+ */
+
+public class DownloadingApi {
+    //for getting comments
+    //for getting photos
+    //for getting todos
+    //for getting posts
+}
---
+++ app/src/main/java/com/developer/r47/carnotmachinetest/utilities/DownloadingUtility.java
@@ -0,0 +1,14 @@
+package com.developer.r47.carnotmachinetest.utilities;
+
+/**
+ * Created by r47 on 29/10/17.
+ * This class implements the methods that DownloadingApi class defines.
+ * This class receives the data from the server and will pass on the data to the activity
+ */
+
+public class DownloadingUtility {
+    //for getting the comments from the server
+    //for getting the photos from the server
+    //for getting the todos from the server
+    //for getting the posts from the server
+}
