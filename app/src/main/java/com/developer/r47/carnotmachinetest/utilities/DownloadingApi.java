b37abee2eaaf7eeb0f9b2d25a920710e33104b02
Rajas Ashtikar <r47@Rajass-MacBook-Pro.local>
2017-10-29 16:32:06 +0530

    Added the endpoints to the DownloadingApi interface

--- app/src/main/java/com/developer/r47/carnotmachinetest/utilities/DownloadingApi.java
+++ app/src/main/java/com/developer/r47/carnotmachinetest/utilities/DownloadingApi.java
@@ -1,14 +1,35 @@
 package com.developer.r47.carnotmachinetest.utilities;
 
+import com.developer.r47.carnotmachinetest.models.Comment;
+import com.developer.r47.carnotmachinetest.models.Photo;
+import com.developer.r47.carnotmachinetest.models.Post;
+import com.developer.r47.carnotmachinetest.models.Todo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by r47 on 29/10/17.
- * This class is the api class, this contains the endpoints that the application will
+ * This interface is the api class, this contains the endpoints that the application will
  * hit, in order to get the required data from the server
  */
 
-public class DownloadingApi {
+public interface DownloadingApi {
     //for getting comments
+    @GET("comments")
+    Call<List<Comment>> getCommentsFromServer();
+
     //for getting photos
+    @GET("photos")
+    Call<List<Photo>> getPhotosFromServer();
+
     //for getting todos
+    @GET("todos")
+    Call<List<Todo>> getTodosFromServer();
+
     //for getting posts
+    @GET("posts")
+    Call<List<Post>> getPostsFromServer();
 }
